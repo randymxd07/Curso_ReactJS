abf36cd34370107a70a56f3ce51ebf03d27df674
Randy Martinez <randym0624@gmail.com>
2022-05-02 00:14:26 -0400

    Aprendi a usar los hooks mas utilizados de react

---
+++ 06 - Hooks Generales/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { Memorize } from './components/06-memos/Memorize';
+// import { MemoHook } from './components/06-memos/MemoHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
+
+
+
+ReactDOM.render(
+    <Padre />,
+  document.getElementById('root')
+);
+
